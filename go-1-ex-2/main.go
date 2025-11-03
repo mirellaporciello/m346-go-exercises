2c3e21f8b8ef4e25d9c5516c95d8fffadf49c306
mirella prociello <mirella_porciello@sluz.ch>
2025-11-03 15:12:16 +0100

    calculated formulas

--- go-1-ex-2/main.go
+++ go-1-ex-2/main.go
@@ -1,22 +1,24 @@
 package main
-
+ 
 import "fmt"
-
+ 
 func main() {
-	const mileInKM = 1.60934
-	var miles = 55.3
-	var kilometres = miles * mileInKM
-	fmt.Printf("%.2f miles = %.2f kilometres\n", miles, kilometres)
-
-	var fahrenheit = 99.9
-	var celsius = (fahrenheit - 32.0) * 5 / 9
-	fmt.Printf("%.2f°F = %.2f°C\n", fahrenheit, celsius)
-
-	const marathonInKM = 42.195
-	var marathonInMiles = 0.0 // TODO: calculate using mileInKM!
-	fmt.Printf("a marathon is %.2f kilometres = %.2f miles long\n", marathonInKM, marathonInMiles)
-
-	var boilingWaterCelsius = 100.0
-	var boilingWaterFahrenheit = 0.0 // TODO: calculate using formula above!
-	fmt.Printf("water boils at %.2f°C = %.2f°F\n", boilingWaterCelsius, boilingWaterFahrenheit)
+    const mileInKM = 1.60934
+    var miles = 55.3
+    var kilometres = miles * mileInKM
+    fmt.Printf("%.2f miles = %.2f kilometres\n", miles, kilometres)
+ 
+    var fahrenheit = 99.9
+    var celsius = (fahrenheit - 32.0) * 5 / 9
+    fmt.Printf("%.2f°F = %.2f°C\n", fahrenheit, celsius)
+ 
+    const marathonInKM = 42.195
+    var marathonInMiles = marathonInKM / mileInKM // TODO: calculate using mileInKM!
+    fmt.Printf("a marathon is %.2f kilometres = %.2f miles long\n", marathonInKM, marathonInMiles)
+ 
+    var boilingWaterCelsius = 100.0
+    var boilingWaterFahrenheit = (boilingWaterCelsius * 9 / 5) + 32 // TODO: calculate using formula above!
+    fmt.Printf("water boils at %.2f°C = %.2f°F\n", boilingWaterCelsius, boilingWaterFahrenheit)
 }
+ 
+ 
