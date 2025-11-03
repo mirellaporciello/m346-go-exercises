85f9f73fc8e458b4d13bc40f80f6bf28245d470c
mirella prociello <mirella_porciello@sluz.ch>
2025-11-03 14:53:02 +0100

    set variable

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -3,6 +3,14 @@ package main
 import "fmt"
 
 func main() {
+firstName := "Mirella"
+    lastName := "Porciello"
+    dayOfBirth := 12
+    monthOfBirth := 4
+    yearOfBirth := 2008
+    numberOfSiblings := 1
+    heightInMeters := 1.50
+    zodiacSign := 'W'
 	// TODO: Declare and initialize the variables being used in the output!
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
