43c3628e3456ed1f84c678b299fcd07285c92026
mirella prociello <mirella_porciello@sluz.ch>
2025-11-10 15:01:02 +0100

    Aufgabe 3

--- go-1-ex-3/main.go
+++ go-1-ex-3/main.go
@@ -1,21 +1,61 @@
 package main
-
+ 
 import (
+
 	"fmt"
+
 	"math/rand"
+
+	"os"
+
 	"time"
+
 )
-
+ 
 func main() {
-	var eyes = rand.Intn(5) + 1
+
+	rand.Seed(time.Now().UnixNano()) // seed the random generator
+
+	var eyes = rand.Intn(6) + 1
+
 	var when = time.Now()
+ 
+	// open or create files
 
-	// TODO: use fmt.Fprintln instead!
-	fmt.Println("the dice shows", eyes, "eyes")
+	eyesFile, err := os.Create("eyes.txt")
 
-	// TODO: use fmt.Fprintln instead!
-	fmt.Println("the dice was rolled at", when)
+	if err != nil {
 
-	// TODO: how to write the output into eyes.txt and dice.log?
-	// go run ex3/main.go TODO
+		fmt.Println("Error creating eyes.txt:", err)
+
+		return
+
+	}
+
+	defer eyesFile.Close()
+ 
+	logFile, err := os.OpenFile("dice.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	if err != nil {
+
+		fmt.Println("Error opening dice.log:", err)
+
+		return
+
+	}
+
+	defer logFile.Close()
+ 
+	// write to files using fmt.Fprintln
+
+	fmt.Fprintln(eyesFile, "the dice shows", eyes, "eyes")
+
+	fmt.Fprintln(logFile, "the dice was rolled at", when)
+ 
+	// optional: also print to console
+
+	fmt.Println("Results written to eyes.txt and dice.log")
+
 }
+
+ 
